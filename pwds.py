50e34195ebbc49bc89e30bc8bee6342ff3886e15
MR-X03X4RN0 <55691025+cacacr4ck@users.noreply.github.com>
2020-08-25 04:36:25 +0700

    Add files via upload

---
+++ pwds.py
@@ -0,0 +1,68 @@
+import os, sys
+
+print ("\033[1;32mLOGIN)")
+
+print ("\033[1;32m")
+
+username = '21'      
+
+password = '24'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mSAMPAI JUMPA", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\032[1;32mSorry Passwordmu salah cok !!!\033[00m"
+
+			print "Back Login\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mSorry..anda noob !!!\033[00m"
+
+		print "Back Login\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
+
