0e7667eb533c5ebd643e7b5ad3d3e78ed281bb7e
MR-X03X4RN0 <55691025+cacacr4ck@users.noreply.github.com>
2020-08-25 04:34:33 +0700

    Import
    #

---
+++ int.sh
@@ -0,0 +1,147 @@
+clear
+figlet "SCrackChanel" | lolcat
+echo "Tunggu Sebentar" | lolcat
+sleep 4
+clear
+figlet -f slant "WELCOME"|lolcat
+echo
+clear
+echo 
+echo "+===============================+" | lolcat
+echo "| Nickname : MR.xoexarno        |" | lolcat
+echo "| WhatsApp : +6283821362840     |" | lolcat
+echo "| Status   : JOMBLO:V           |" | lolcat
+echo "+===============================+" | lolcat
+echo
+toilet -f future "  SANTUYY  " | lolcat
+echo   " Silahkan dipilih : " | lolcat
+echo "+===========================+" | lolcat
+echo "+[1].KUOTA MURAH     |  X  |+" | lolcat
+echo "+[2].DDOS            |  O  |+" | lolcat
+echo "+[3].AUTO REPORT     |  E  |+" | lolcat
+echo "+[4].PROFIL GUARD FB |  X  |+" | lolcat
+echo "+[5].BOT IG          |  A  |+" | lolcat
+echo "+[6].HACK FB TARGET  |  R  |+" | lolcat
+echo "+[7].SPAM WA         |  N  |+" | lolcat
+echo "+[8].SPAM SMS        |  O  |+" | lolcat
+echo "+[F].DarkFb          |  -  |+" | lolcat
+echo "+[H].Mode Hacker     |  -  |+" | lolcat
+echo "+[0].Keluar          |  ツ |+" | lolcat
+echo "+===========================+" | lolcat
+echo "╭─[pilih]" | lolcat
+read -p "╰─>" p
+
+if [ $p = 1 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/sumar-kun/KuotaMurah
+cd KuotaMurah
+sh 5um4r.sh
+fi
+
+if [ $p = 2 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+cd DOS-TOOKITS
+sh DoS-TOOKIT.sh
+fi
+
+if [ $p = 3 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $p = 4 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfilGuardFb
+php guard.php
+fi
+
+if [ $p = 5 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/ikiganteng/bot-igeh
+cd bot-igeh
+unzip node_modules.zip
+node index.js
+fi
+
+if [ $p = 6 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/mbest99/MIXINGS.git
+cd MIXINGS
+bash oppay.sh
+fi
+
+if [ $p = 7 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/TobzCyberTeam/SpamWa
+cd SpamWa
+php SpamWa.php
+fi
+
+if [ $p = 8 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/joss24242/SpamSms
+cd SpamSms
+python2 mantan.py
+fi
+
+if [ $p = 9 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+git clone https://github.com/Ranginang67/DarkFly
+cd DarkFly
+sh DarkFly.sh
+DarkFly
+fi
+
+if [ $p = H ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep 1
+pkg install cmatrix
+clear
+cmatrix
+fi
+
+if [ $p = 0 ]
+then
+clear
+figlet -f slant "EXIT"|lolcat
+echo "Thanks telah menggunakan tools ini"
+echo 
+echo "SeeYouAgain"
+echo "Dont Forget Coli;)"
+sleep 3
+exit
+fi
